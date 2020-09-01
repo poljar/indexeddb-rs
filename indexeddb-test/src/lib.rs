37319626a850d281075c951a7527663103267afd
Damir Jelić <poljar@termina.org.uk>
2020-09-01 14:00:11 +0200

    indexeddb-test: Update the test.

--- indexeddb-test/src/lib.rs
+++ indexeddb-test/src/lib.rs
@@ -1,33 +1,32 @@
 use console_web::println;
-use futures::Future;
+use indexeddb::IndexedDb;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+
+async fn main(version: u32) {
+    let db = IndexedDb::open("test", version, move |_old_version, db| {
+        if version >= 1 {
+            let _store = db.create_object_store("contact", "id", true).unwrap();
+            // store
+            //     .create_index("idx_given_name", "given_name", false)
+            //     .unwrap();
+            // store
+            //     .create_index("idx_family_name", "family_name", false)
+            //     .unwrap();
+        }
+    })
+    .await;
+
+    match db {
+        Ok(ref db) => println!("Success: {:?}", db),
+        Err(ref e) => println!("Error: {:?}", e),
+    }
+}
 
 #[wasm_bindgen(start)]
 pub fn run() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     let version = 1;
-    spawn_local(
-        indexeddb::open("test", version, move |old_version, db| {
-            if version >= 1 {
-                let store = db.create_object_store("contact", "id", true).unwrap();
-                store
-                    .create_index("idx_given_name", "given_name", false)
-                    .unwrap();
-                store
-                    .create_index("idx_family_name", "family_name", false)
-                    .unwrap();
-            }
-        })
-        .then(|res| {
-            match res {
-                Ok(ref db) => println!("Success: {:?}", db),
-                Err(ref e) => println!("Error: {:?}", e),
-            }
-            res
-        }),
-    );
-}
 
-pub fn spawn_local(future: impl Future + 'static) {
-    wasm_bindgen_futures::spawn_local(future.map(|_| ()).map_err(|_| ()));
+    spawn_local(main(version));
 }
